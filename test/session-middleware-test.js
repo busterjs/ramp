516f88f34b56d800d78c098259c1cbcdd7efa823
August Lilleaas <august.lilleaas@shortcut.no>
2011-04-03 20:03:28 +0200

    Session emits event when session is created.
    Moving all existing session tests to new context, top level context no longer has a session precreated.

--- test/session-middleware-test.js
+++ test/session-middleware-test.js
@@ -16,24 +16,20 @@ buster.testCase("Session middleware", {
                 res.end();
             };
         });
-        this.httpServer.listen(h.SERVER_PORT, function (e) {
-            h.request({path: "/sessions", method: "POST"}, function (res, body) {
-                self.res = res;
-                self.session = JSON.parse(body);
-                done();
-            }).end(new Buffer(JSON.stringify({
-                load: ["/foo.js"],
-                resources: {
-                    "/foo.js": {
-                        content: "5 + 5;"
-                    },
-                    "/bar/baz.js": {
-                        content: "5 + 5;",
-                        headers: {"Content-Type": "text/custom"}
-                    }
+        this.httpServer.listen(h.SERVER_PORT, done);
+
+        this.validSessionPayload = new Buffer(JSON.stringify({
+            load: ["/foo.js"],
+            resources: {
+                "/foo.js": {
+                    content: "5 + 5;"
+                },
+                "/bar/baz.js": {
+                    content: "5 + 5;",
+                    headers: {"Content-Type": "text/custom"}
                 }
-            }), "utf8"));
-        });
+            }
+        }), "utf8")
     },
 
     tearDown: function (done) {
@@ -41,102 +37,124 @@ buster.testCase("Session middleware", {
         this.httpServer.close();
     },
 
-    "test creating session": function () {
-        buster.assert.equals(201, this.res.statusCode);
-        buster.assert("location" in this.res.headers);
-        buster.assert.match(this.res.headers.location, /^\/.+/);
-
-        buster.assert("rootPath" in this.session);
-        buster.assert.equals(this.res.headers.location, this.session.rootPath);
-
-        buster.assert("resourceContextPath" in this.session);
-        // resourceContextPath should be prefixed with rootPath.
-        var expectedPrefix = this.session.resourceContextPath.slice(0, this.session.rootPath.length)
-        buster.assert.equals(expectedPrefix, this.session.rootPath);
-    },
-
-    "test hosts resources": function (done) {
-        h.request({path: this.session.resourceContextPath + "/foo.js", method: "GET"}, function (res, body) {
-            buster.assert.equals(200, res.statusCode);
-            buster.assert.equals("5 + 5;", body);
-            buster.assert.equals("text/javascript", res.headers["content-type"]);
+    "test emits event with session info when creating session": function (done) {
+        var sessionStart = sinon.spy();
+        this.sessionMiddleware.on("session:start", sessionStart);
+        h.request({path: "/sessions", method: "POST"}, function (res, body) {
+            buster.assert(sessionStart.calledOnce);
+            var sessionInfo = sessionStart.getCall(0).args[0];
+            buster.assert.equals("object", typeof(sessionInfo));
             done();
-        }).end();
+        }).end(this.validSessionPayload);
     },
 
-    "test hosts resources with custom headers": function (done) {
-        h.request({path: this.session.resourceContextPath + "/bar/baz.js", method: "GET"}, function (res, body) {
-            buster.assert.equals(200, res.statusCode);
-            buster.assert.equals("text/custom", res.headers["content-type"]);
-            done();
-        }).end();
-    },
+    "with created session": {
+        setUp: function (done) {
+            var self = this;
+            h.request({path: "/sessions", method: "POST"}, function (res, body) {
+                self.res = res;
+                self.session = JSON.parse(body);
+                done();
+            }).end(this.validSessionPayload);
+        },
 
-    "test provides default root resource": function (done) {
-        h.request({path: this.session.resourceContextPath + "/", method: "GET"}, function (res, body) {
-            buster.assert.equals(200, res.statusCode);
-            buster.assert.equals("text/html", res.headers["content-type"]);
-            done();
-        }).end();
-    },
+        "test creating session": function () {
+            buster.assert.equals(201, this.res.statusCode);
+            buster.assert("location" in this.res.headers);
+            buster.assert.match(this.res.headers.location, /^\/.+/);
 
-    "test does not serve none existing resources": function (done) {        
-        h.request({path: this.session.resourceContextPath + "/does/not/exist.js", method: "GET"}, function (res, body) {
-            buster.assert.equals(h.NO_RESPONSE_STATUS_CODE, res.statusCode);
-            done();
-        }).end();
-    },
+            buster.assert("rootPath" in this.session);
+            buster.assert.equals(this.res.headers.location, this.session.rootPath);
 
-    "test provides session environment script": function (done) {
-        var self = this;
-        h.request({path: this.session.rootPath + "/env.js", method: "GET"}, function (res, body) {
-            buster.assert.equals(200, res.statusCode);
-            buster.assert.equals("text/javascript", res.headers["content-type"]);
+            buster.assert("resourceContextPath" in this.session);
+            // resourceContextPath should be prefixed with rootPath.
+            var expectedPrefix = this.session.resourceContextPath.slice(0, this.session.rootPath.length)
+            buster.assert.equals(expectedPrefix, this.session.rootPath);
+        },
 
-            var ctx = {};
-            vm.runInNewContext(body, ctx);
-            buster.assert.equals("object", typeof ctx.buster);
-            buster.assert.equals(self.session.rootPath, ctx.buster.rootPath);
-            buster.assert.equals(self.session.resourceContextPath, ctx.buster.resourceContextPath);
+        "test hosts resources": function (done) {
+            h.request({path: this.session.resourceContextPath + "/foo.js", method: "GET"}, function (res, body) {
+                buster.assert.equals(200, res.statusCode);
+                buster.assert.equals("5 + 5;", body);
+                buster.assert.equals("text/javascript", res.headers["content-type"]);
+                done();
+            }).end();
+        },
 
-            done();
-        }).end();
-    },
+        "test hosts resources with custom headers": function (done) {
+            h.request({path: this.session.resourceContextPath + "/bar/baz.js", method: "GET"}, function (res, body) {
+                buster.assert.equals(200, res.statusCode);
+                buster.assert.equals("text/custom", res.headers["content-type"]);
+                done();
+            }).end();
+        },
 
-    "test inserts buster scripts and session scripts into root resource": function (done) {
-        var self = this;
-        h.request({path: this.session.resourceContextPath + "/", method: "GET"}, function (res, body) {
-            buster.assert.match(body, '<script src="' + self.session.rootPath  + '/env.js"');
-            buster.assert.match(body, '<script src="' + self.session.resourceContextPath  + '/foo.js"');
-            done();
-        }).end();
-    },
+        "test provides default root resource": function (done) {
+            h.request({path: this.session.resourceContextPath + "/", method: "GET"}, function (res, body) {
+                buster.assert.equals(200, res.statusCode);
+                buster.assert.equals("text/html", res.headers["content-type"]);
+                done();
+            }).end();
+        },
 
-    "test killing sessions": function (done) {
-        var self = this;
-        var sessionEnd = sinon.spy();
-        this.sessionMiddleware.on("session:end", sessionEnd);
-        h.request({path: this.session.rootPath, method: "DELETE"}, function (res, body) {
-            buster.assert.equals(200, res.statusCode);
-            buster.assert(sessionEnd.calledOnce);
-
-            h.request({path: self.session.resourceContextPath + "/foo.js", method: "GET"}, function (res, body) {
+        "test does not serve none existing resources": function (done) {        
+            h.request({path: this.session.resourceContextPath + "/does/not/exist.js", method: "GET"}, function (res, body) {
                 buster.assert.equals(h.NO_RESPONSE_STATUS_CODE, res.statusCode);
-                h.request({path: self.session.rootPath, method: "GET"}, function (res, body) {
+                done();
+            }).end();
+        },
+
+        "test provides session environment script": function (done) {
+            var self = this;
+            h.request({path: this.session.rootPath + "/env.js", method: "GET"}, function (res, body) {
+                buster.assert.equals(200, res.statusCode);
+                buster.assert.equals("text/javascript", res.headers["content-type"]);
+
+                var ctx = {};
+                vm.runInNewContext(body, ctx);
+                buster.assert.equals("object", typeof ctx.buster);
+                buster.assert.equals(self.session.rootPath, ctx.buster.rootPath);
+                buster.assert.equals(self.session.resourceContextPath, ctx.buster.resourceContextPath);
+
+                done();
+            }).end();
+        },
+
+        "test inserts buster scripts and session scripts into root resource": function (done) {
+            var self = this;
+            h.request({path: this.session.resourceContextPath + "/", method: "GET"}, function (res, body) {
+                buster.assert.match(body, '<script src="' + self.session.rootPath  + '/env.js"');
+                buster.assert.match(body, '<script src="' + self.session.resourceContextPath  + '/foo.js"');
+                done();
+            }).end();
+        },
+
+        "test killing sessions": function (done) {
+            var self = this;
+            var sessionEnd = sinon.spy();
+            this.sessionMiddleware.on("session:end", sessionEnd);
+            h.request({path: this.session.rootPath, method: "DELETE"}, function (res, body) {
+                buster.assert.equals(200, res.statusCode);
+                buster.assert(sessionEnd.calledOnce);
+
+                h.request({path: self.session.resourceContextPath + "/foo.js", method: "GET"}, function (res, body) {
                     buster.assert.equals(h.NO_RESPONSE_STATUS_CODE, res.statusCode);
-                    done();
+                    h.request({path: self.session.rootPath, method: "GET"}, function (res, body) {
+                        buster.assert.equals(h.NO_RESPONSE_STATUS_CODE, res.statusCode);
+                        done();
+                    }).end();
                 }).end();
             }).end();
-        }).end();
-    },
+        },
 
-    "test creating session with other session in progress": function (done) {
-        h.request({path: "/sessions", method: "POST"}, function (res, body) {
-            buster.assert.equals(202, res.statusCode);
-            done();
-        }).end(new Buffer(JSON.stringify({
-            load: [],
-            resources: {"/foo.js": {content: "5 + 5;"}}
-        }), "utf8"));
+        "test creating session with other session in progress": function (done) {
+            h.request({path: "/sessions", method: "POST"}, function (res, body) {
+                buster.assert.equals(202, res.statusCode);
+                done();
+            }).end(new Buffer(JSON.stringify({
+                load: [],
+                resources: {"/foo.js": {content: "5 + 5;"}}
+            }), "utf8"));
+        }
     }
 });
