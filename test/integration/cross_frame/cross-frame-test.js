2f7cbb7f8a1919efb122922bfcaa705c95b8b782
August Lilleaas <august@augustl.com>
2011-04-14 19:50:15 +0200

    Adding on load listener to cross frame lib.

--- test/integration/cross_frame/cross-frame-test.js
+++ test/integration/cross_frame/cross-frame-test.js
@@ -1,6 +1,13 @@
 (function () {
     var cf = buster.create(buster.server.crossFrame);
     cf.targetFrameId = "target_frame";
-
     cf.document().getElementById("test_element").innerHTML = "Test passed, yay for " + cf.window()["Infinity"] + ".";
+
+
+    var cf = buster.create(buster.server.crossFrame);
+    cf.targetFrameId = "other_frame";
+    cf.addOnLoadListener(function () {
+        cf.document().getElementById("target").innerHTML = "Yay, load event worked";
+    });
+    cf.frame().src = "_event.html";
 }());
