2ade244e7fc0f80c7a41aabfc7260e1e824e8a88
August Lilleaas <august@augustl.com>
2011-04-16 13:02:47 +0200

    Adding buster.clientReady callback, for the client frame to call on the control frame to let everyone know the client is ready.
    This causes the control frame to expose the multicast client to the client frame.

--- test/browser/control-frame-test.js
+++ test/browser/control-frame-test.js
@@ -1,11 +1,16 @@
 (function () {
     function mockCrossFrame() {
         var frame = {};
-        return function () {
+        var window = {};
+        var result = function () {
             return {
-                frame: function () { return frame; }
+                frame: function () { return frame; },
+                window: function () { return window; }
             }
         }
+        result._window = window;
+
+        return result;
     }
 
     function mockMulticastClient() {
@@ -16,6 +21,8 @@
         setUp: function () {
             this.cf = buster.create(buster.server.controlFrame);
             this.sandbox = sinon.sandbox.create();
+
+            delete buster.clientReady;
 
             this.env = {};
             this.originalEnv = buster.env;
@@ -60,16 +67,33 @@
             assert(this.cf.sessionEnd.calledWithExactly(event));
         },
 
-        "test sessionStart sets src on the client frame to the root resource": function () {
+        "test sessionStart": function () {
             this.cf.crossFrame = mockCrossFrame();
             this.cf.sessionStart({data: {resourceContextPath: "/foo"}});
             assertEquals("/foo/", this.cf.crossFrame().frame().src);
+
+            assertEquals(typeof(buster.clientReady), "function");
+
+            this.sandbox.stub(this.cf, "exposeBusterObject");
+            buster.clientReady();
+            assert(this.cf.exposeBusterObject.calledOnce);
         },
 
         "test sessionEnd blanks src on the client frame": function () {
             this.cf.crossFrame = mockCrossFrame();
             this.cf.sessionEnd({});
             assertEquals("", this.cf.crossFrame().frame().src);
+        },
+
+        "test exposeBusterObject": function () {
+            this.cf.crossFrame = mockCrossFrame();
+            // When this functino is called, buster is already defined on the
+            // cross frame window object.
+            this.cf.crossFrame._window.buster = {};
+            this.cf.multicastClient = {};
+            this.cf.exposeBusterObject();            
+
+            assertSame(this.cf.crossFrame().window().buster.multicastClient, this.cf.multicastClient);
         },
 
         "test crossFrame": function () {
