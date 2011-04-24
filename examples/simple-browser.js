3fc7eaea720428e5652523ceb449d634a13bf3c7
August Lilleaas <august@augustl.com>
2011-04-24 02:05:38 +0200

    Adding IE compatible event listeners to example.

--- examples/simple-browser.js
+++ examples/simple-browser.js
@@ -1,11 +1,23 @@
 (function () {
+    function bindEvent(element, event, handler) {
+        if (element.attachEvent) {
+            element.attachEvent("on" + event, handler);
+            return;
+        }
+
+        if (element.addEventListener) {
+            element.addEventListener(event, handler, false);
+            return;
+        }
+    }
+
     var indicator = document.createElement("h1");
     indicator.innerHTML = "Press a key on your keyboard.";
     document.body.appendChild(indicator);
 
-    window.addEventListener("keyup", function (e) {
+    bindEvent(window, "keyup", function (e) {
         buster.multicastClient.emit("update", e.keyCode);
-    }, false);
+    });
 
     buster.multicastClient.on("update", function (event) {
         indicator.innerHTML += String.fromCharCode(event.data);
