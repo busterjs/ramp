d730dd7a1343a6ba48de272eb9a56f3479c924f3
August Lilleaas <august@augustl.com>
2011-08-31 23:09:44 +0200

    Adding attach method, an alternative to simply calling respond().
    This enables us to use 3rd party libraries (faye) that also attaches via an attach method, without forcing buster-server users to use both respond and attach.

--- test/buster-server-test.js
+++ test/buster-server-test.js
@@ -7,27 +7,77 @@ var http = require("http");
 var h = require("./test-helper");
 
 buster.testCase("buster-server glue", {
-    setUp: function (done) {
-        var self = this;
-        this.server = busterServer.create();
-        this.httpServer = http.createServer(function (req, res) {
-            if (!self.server.respond(req, res)) {
+    "responding on existing server": {
+        setUp: function (done) {
+            var self = this;
+            this.server = busterServer.create();
+            this.httpServer = http.createServer(function (req, res) {
+                if (!self.server.respond(req, res)) {
+                    res.writeHead(h.NO_RESPONSE_STATUS_CODE);
+                    res.end();
+                }
+            });
+            this.httpServer.listen(h.SERVER_PORT, done);
+        },
+
+        tearDown: function (done) {
+            this.httpServer.on("close", done);
+            this.httpServer.close();
+        },
+
+        "test unknown URL": function (done) {
+            h.request({path: "/doesnotexist", method: "GET"}, function (res, body) {
+                assert.equals(h.NO_RESPONSE_STATUS_CODE, res.statusCode);
+                done();
+            }).end();
+        }
+    },
+
+    "attaching to existing server": {
+        setUp: function (done) {
+            this.server = busterServer.create();
+            this.httpServer = http.createServer(function (req, res) {
                 res.writeHead(h.NO_RESPONSE_STATUS_CODE);
                 res.end();
-            }
-        });
-        this.httpServer.listen(h.SERVER_PORT, done);
-    },
+            });
+            this.httpServer.listen(h.SERVER_PORT, done);
+        },
 
-    tearDown: function (done) {
-        this.httpServer.on("close", done);
-        this.httpServer.close();
-    },
+        tearDown: function (done) {
+            this.httpServer.on("close", done);
+            this.httpServer.close();
+        },
 
-    "test unknown URL": function (done) {
-        h.request({path: "/doesnotexist", method: "GET"}, function (res, body) {
-            assert.equals(h.NO_RESPONSE_STATUS_CODE, res.statusCode);
-            done();
-        }).end();
+        "keeps listener created when creating server": function (done) {
+            this.server.attach(this.httpServer);
+            var spy = this.spy(this.server, "respond");
+            h.request({path: "/doesnotexist", method: "GET"}, function (res, body) {
+                assert.equals(h.NO_RESPONSE_STATUS_CODE, res.statusCode);
+                assert(spy.calledOnce);
+                done();
+            }).end();
+        },
+
+        "keeps listener added after creating server and before attaching": function (done) {
+            var spy = this.spy(this.server, "respond");
+            this.httpServer.addListener("request", function (req, res) {
+                assert(spy.calledOnce);
+                done();
+            });
+            this.server.attach(this.httpServer);
+
+            h.request({path: "/doesnotexist", method: "GET"}, function (res, body) {}).end();
+        },
+
+        "keeps listener added after creating server and after attaching": function (done) {
+            this.server.attach(this.httpServer);
+            var spy = this.spy(this.server, "respond");
+            this.httpServer.addListener("request", function (req, res) {
+                assert(spy.calledOnce);
+                done();
+            });
+
+            h.request({path: "/doesnotexist", method: "GET"}, function (res, body) {}).end();
+        }
     }
 });
