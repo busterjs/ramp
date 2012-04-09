ed4dcfd223bbe7474d04bbf384ef6b453b689f7e
August Lilleaas <august@augustl.com>
2012-04-10 01:26:50 +0200

    Improving implementation of extend to use always bound methods, so that the extended method and the actual pubsub client object methods are the same objects.

--- test/pubsub-client-test.js
+++ test/pubsub-client-test.js
@@ -215,33 +215,11 @@ buster.testCase("pubsub-client", {
 
     "extending": function () {
         var obj = {};
-        var arg = "foo";
-        this.stub(this.ps, "connect");
-        this.stub(this.ps, "disconnect");
-        this.stub(this.ps, "on");
-        this.stub(this.ps, "emit");
-
         this.ps.extend(obj);
-
-        obj.connect(arg);
-        assert.calledOnce(this.ps.connect);
-        assert.same(this.ps.connect.getCall(0).args[0], arg);
-        assert.same(this.ps.connect.getCall(0).thisValue, this.ps);
-
-        obj.disconnect(arg);
-        assert.calledOnce(this.ps.disconnect);
-        assert.same(this.ps.disconnect.getCall(0).args[0], arg);
-        assert.same(this.ps.disconnect.getCall(0).thisValue, this.ps);
-
-        obj.on(arg);
-        assert.calledOnce(this.ps.on);
-        assert.same(this.ps.on.getCall(0).args[0], arg);
-        assert.same(this.ps.on.getCall(0).thisValue, this.ps);
-
-        obj.emit(arg);
-        assert.calledOnce(this.ps.emit);
-        assert.same(this.ps.emit.getCall(0).args[0], arg);
-        assert.same(this.ps.emit.getCall(0).thisValue, this.ps);
+        assert.same(this.ps.connect, obj.connect);
+        assert.same(this.ps.disconnect, obj.disconnect);
+        assert.same(this.ps.emit, obj.emit);
+        assert.same(this.ps.on, obj.on);
     },
 
     "should call onConnect when donnecting": function (done) {
