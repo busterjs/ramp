9ae1f291af4d91a89cdcc96c35526f62268231e8
August Lilleaas <august@augustl.com>
2012-04-10 02:45:46 +0200

    Notifing server client when session queue captures and frees slavs.

--- test/server-test.js
+++ test/server-test.js
@@ -12,17 +12,19 @@ buster.testCase("server", {
         this.httpServer = http.createServer(function (req, res) {
             res.writeHead(h.NO_RESPONSE_STATUS_CODE); res.end();
         });
-        this.httpServer.listen(h.SERVER_PORT, done);
+        this.httpServer.listen(h.SERVER_PORT, function () {
+            this.c = bCaptureServer.createServerClient("0.0.0.0", h.SERVER_PORT);
+            this.c.connect().then(done);
+        }.bind(this));
 
         this.s = bCaptureServer.createServer();
         this.s.attach(this.httpServer);
-
-        this.c = bCaptureServer.createServerClient("0.0.0.0", h.SERVER_PORT);
     },
 
     tearDown: function (done) {
         this.httpServer.on("close", done);
         this.httpServer.close();
+        this.c.disconnect();
     },
 
     "should create new session successfully": function (done) {
@@ -40,6 +42,28 @@ buster.testCase("server", {
                 assert.match(err.message, "unknown property");
             })
         );
+    },
+
+    "emits event when session queue emits slave:captured": function (done) {
+        this.c.on("slave:captured", done(function (e) {
+            assert.equals(e, "foo");
+        }));
+
+        // TODO: Meh, timeout
+        setTimeout(function () {
+            this.s.sessionQueue.emit("slave:captured", "foo");
+        }.bind(this), 100);
+    },
+
+    "emits event when session queue emits slave:freed": function (done) {
+        this.c.on("slave:freed", done(function (e) {
+            assert.equals(e, "foo");
+        }));
+
+        // TODO: Meh, timeout
+        setTimeout(function () {
+            this.s.sessionQueue.emit("slave:freed", "foo");
+        }.bind(this), 100);
     },
 
     "// should fail if attempting to load uncached items": function () {
