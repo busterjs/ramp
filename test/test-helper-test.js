00ff420ad0dfb78655e5e4eeaf14fa21a89374d0
August Lilleaas <august@augustl.com>
2012-10-14 10:22:42 +0200

    Adding test coverage for the public test-helper in lib/.
    Fixing a bug, the test helper needs to send the pubsub client ID to the server.

---
+++ test/test-helper-test.js
@@ -0,0 +1,31 @@
+var buster = require("buster");
+var assert = buster.assert;
+var refute = buster.refute;
+
+var h = require("./helpers/test-helper");
+var bCapServTestHelper = require("../lib/buster-capture-server").testHelper;
+
+buster.testCase("Test helper", {
+    setUp: function (done) {
+        this.serverBundle = h.createServerBundle(0, this, done);
+        this.timeout = 4000;
+    },
+
+    tearDown: function (done) {
+        this.serverBundle.tearDown(done);
+    },
+
+    "should capture a slave against an actual server": function (done) {
+        var actualUA = "My user agent";
+        var promise = bCapServTestHelper.captureSlave(this.port, actualUA);
+
+        promise.then(done(function (e) {
+            assert(e);
+            assert.equals(e.slaves, [e.slave]);
+
+            var slave = e.slave;
+            assert(slave);
+            assert.equals(slave.userAgent, actualUA);
+        }));
+    }
+});
