760fcdfbf9bdab440ef7886430f226d0ce81e07a
August Lilleaas <august@augustl.com>
2013-08-28 23:04:40 +0200

    Removing all the old code.

--- run-tests.js
+++
@@ -1,8 +0,0 @@
-var buster = require("buster-node");
-require("./test/cache-test");
-require("./test/events-test");
-require("./test/joinable-and-unjoinable-test");
-require("./test/main-test");
-require("./test/session-lifecycle-test");
-require("./test/slave-header-test");
-require("./test/test-helper-test");
