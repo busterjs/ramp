1073d7eb108db460db82dc7fe3ed864d35e59a59
August Lilleaas <august@augustl.com>
2013-09-04 01:27:46 +0200

    Recapturing slaves when server restarts.

--- run-tests.js
+++ run-tests.js
@@ -3,3 +3,4 @@ buster.testRunner.timeout = 4000;
 
 require("./test/session-test")
 require("./test/slave-test")
+require("./test/slave-reloading-on-server-restart-test")
---
+++ test/slave-reloading-on-server-restart-test.js
@@ -0,0 +1,49 @@
+var buster = require("buster-node");1
+var assert = buster.assert;
+var refute = buster.refute;
+
+var ramp = require("./../lib/ramp");
+var th = require("./test-helper.js");
+
+buster.testCase("Slave reloading on server restart", {
+    setUp: function () {
+        return th.setUpHelpers(this, [th.ph]);
+    },
+
+    tearDown: function () {
+        return th.tearDownHelpers(this);
+    },
+
+    "should work": function (done) {
+        var self = this;
+
+        th.spawnServer(0, function (port, rampServerUrl, process) {
+            // Pretending to be th.rs.
+            self.rs = {captureUrl: rampServerUrl + "/capture", port: port};
+
+            th.capture(self, function (rc, page) {
+                process.kill("SIGKILL");
+                process.on("exit", function () {
+                    th.spawnServer(port, function (port, rampServerUrl, process) {
+                        var rc2 = ramp.createRampClient(port);
+
+                        function tryGettingSlaves() {
+                            rc2.getSlaves().then(function (slaves) {
+                                if (slaves.length === 1) {
+                                    assert(true);
+                                    done();
+                                } else {
+                                    tryGettingSlaves();
+                                }
+                            }, function () {
+                                tryGettingSlaves();
+                            });
+                        };
+
+                        tryGettingSlaves();
+                    });
+                })
+            });
+        });
+    }
+})
