c22deea951c44757a919415a75227414fa405a6f
August Lilleaas <august@augustl.com>
2013-09-04 18:22:51 +0200

    Setting header properly.

---
+++ test/slave-header-test.js
@@ -0,0 +1,77 @@
+var buster = require("buster-node");1
+var assert = buster.assert;
+var refute = buster.refute;
+
+var ramp = require("./../lib/ramp");
+var http = require("http");
+var rampResources = require("ramp-resources");
+var th = require("./test-helper.js");
+var when_pipeline = require("when/pipeline");
+
+buster.testCase("Slave header", {
+    setUp: function (done) {
+        var self = this;
+
+        var httpServer = http.createServer(function (req, res) {
+            res.writeHead(418);
+            res.end();
+        })
+
+        httpServer.listen(0, function () {
+            self.httpServer = httpServer;
+            self.httpServerPort = httpServer.address().port;
+
+            var rs = rampResources.createResourceSet();
+            rs.addResource({
+                path: "/",
+                content: 'Hello, World!'
+            });
+
+            var rampServer = ramp.createRampServer({
+                header: {
+                    resourceSet: rs,
+                    height: 80
+                }
+            });
+            rampServer.attach(httpServer);
+            self.rampServer = rampServer;
+
+            self.rc = ramp.createRampClient(self.httpServerPort);
+            done();
+        });
+    },
+
+    tearDown: function (done) {
+        this.httpServer.close(done);
+    },
+
+    "should be present": function (done) {
+        var self = this;
+        var serverUrl = "http://localhost:" + this.httpServerPort;
+
+        th.promiseSuccess(
+            when_pipeline([
+                function () {
+                    return th.httpGet(serverUrl + "/capture")
+                },
+                function (e) {
+                    assert.equals(e.res.statusCode, 302);
+                    return th.httpGet(serverUrl + e.res.headers.location);
+                },
+                function (e) {
+                    assert.equals(e.res.statusCode, 200);
+                    assert.equals(e.body.match(/\<frame[^s]\s/ig).length, 2, "Should find two frame tags");
+                    assert.match(e.body, /80px/);
+                    assert.match(e.body, /\/slave_header\//);
+                },
+                function () {
+                    return th.httpGet(serverUrl + "/slave_header/");
+                },
+                function (e) {
+                    assert.equals(e.res.statusCode, 200);
+                    assert.equals(e.body , "Hello, World!");
+                }
+            ]),
+            done);
+    }
+})
