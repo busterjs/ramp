2257164d5bc82ad16e5392fd93b206e3233e1cfa
August Lilleaas <august@augustl.com>
2013-09-04 23:18:35 +0200

    Also adding graceful disconnect of ramp client.

--- test/ramp-client-death-test.js
+++ test/ramp-client-death-test.js
@@ -8,6 +8,23 @@ var when = require("when");
 var when_pipeline = require("when/pipeline");
 var cp = require("child_process");
 
+function pollForSession(port, pred) {
+    var rc = ramp.createRampClient(port);
+    var deferred = when.defer();
+    var poll = function () {
+        rc.getCurrentSession().then(function (session) {
+            var result = pred(session);
+            if (result === undefined) {
+                poll();
+            } else {
+                deferred.resolve(result);
+            }
+        }, deferred.reject);
+    };
+    poll();
+    return deferred.promise;
+}
+
 buster.testCase("Ramp client death", {
     setUp: function () {
         return th.setUpHelpers(this, [th.rs]);
@@ -18,38 +35,49 @@ buster.testCase("Ramp client death", {
     },
 
     "should end session": function (done) {
+        var self = this;
+
         var rcproc = cp.spawn("node", [__dirname + "/ramp-client-with-session-loader.js", this.rs.port]);
         rcproc.stdout.pipe(process.stdout);
         rcproc.stderr.pipe(process.stderr);
 
         var rc = ramp.createRampClient(this.rs.port);
 
-        function pollForSession(pred) {
-            var deferred = when.defer();
-            var poll = function () {
-                rc.getCurrentSession().then(function (session) {
-                    var result = pred(session);
-                    if (result === undefined) {
-                        poll();
-                    } else {
-                        deferred.resolve(result);
-                    }
-                }, deferred.reject);
-            };
-            poll();
-            return deferred.promise;
-        }
-
         th.promiseSuccess(
             when_pipeline([
                 function () {
-                    return pollForSession(function (session) { if (session) return session })
+                    return pollForSession(self.rs.port, function (session) { if (session) return session })
                 },
                 function (session) {
                     rcproc.kill("SIGKILL");
                 },
                 function (session) {
-                    return pollForSession(function (session) { if (!session) return null });
+                    return pollForSession(self.rs.port, function (session) { if (!session) return null });
+                },
+                function () {
+                    assert(true)
+                }
+            ]),
+            done);
+    },
+
+    "should end session when graceful": function (done) {
+        var self = this;
+
+        var rcproc = cp.spawn("node", [__dirname + "/ramp-client-with-session-loader.js", this.rs.port]);
+        rcproc.stdout.pipe(process.stdout);
+        rcproc.stderr.pipe(process.stderr);
+
+        th.promiseSuccess(
+            when_pipeline([
+                function () {
+                    return pollForSession(self.rs.port, function (session) { if (session) return session })
+                },
+                function (session) {
+                    rcproc.kill("SIGINT");
+                },
+                function (session) {
+                    return pollForSession(self.rs.port, function (session) { if (!session) return null });
                 },
                 function () {
                     assert(true)
